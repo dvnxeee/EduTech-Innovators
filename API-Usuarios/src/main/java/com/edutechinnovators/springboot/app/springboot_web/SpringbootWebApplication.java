09c8fc1977b0baf5b71ac6b1f50421b32c6c4aaf
jhonnylarry <jonathanlarraguibel@gmail.com>
2025-05-26 22:42:23 -0400

    Actualiza SpringbootWebApplication.java

--- API-Usuarios/src/main/java/com/edutechinnovators/springboot/app/springboot_web/SpringbootWebApplication.java
+++ API-Usuarios/src/main/java/com/edutechinnovators/springboot/app/springboot_web/SpringbootWebApplication.java
@@ -2,11 +2,8 @@ package com.edutechinnovators.springboot.app.springboot_web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
 
-//Esta anotación indica que esta clase es la clase principal de configuración de Spring Boot
-//Spring Boot usa esta clase para iniciar y configurar todo el contexto de la aplicación automáticamente
+
 @SpringBootApplication
 public class SpringbootWebApplication {
 
@@ -18,15 +15,5 @@ public class SpringbootWebApplication {
         SpringApplication.run(SpringbootWebApplication.class, args);
     }
 
-    //Definición de un Bean que se registrará en el contexto de Spring
-    //Un Bean es un componente gestionado por Spring que puede ser inyectado y reutilizado
-    //En este caso, el bean es un filtro HTTP que permite que los formularios HTML puedan enviar peticiones
-    //con métodos HTTP como PUT, DELETE o PATCH usando un campo oculto "_method" en el formulario
-    //Esto es necesario porque los formularios HTML estándar solo soportan GET y POST por defecto
-    //Así, al incluir este filtro, puedo manejar correctamente las solicitudes HTTP con esos métodos "ocultos"
-    @Bean
-    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
-        //Retorno una instancia del filtro HiddenHttpMethodFilter que Spring usará automáticamente
-        return new HiddenHttpMethodFilter();
-    }
+    
 }
